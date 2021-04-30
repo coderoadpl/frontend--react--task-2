67c999d1a033a32cc0c2acd46a05e6aee1b5af28
Mateusz Choma <kontakt@coderoad.pl>
2021-04-30 09:07:28 +0200

    React element compared to document API

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const firstElement = React.createElement(
     'h1',
     {},
-     ['Hello React']   
+    ['Hello React', 'Hello React 2']
 )
 
 ReactDOM.render(
     firstElement,
-    document.getElementById('root')
+    document.getElementById('root-react')
 )
+
+const firstElementDom = document.createElement('h1')
+const textDom = document.createTextNode('Hello React')
+const textDom2 = document.createTextNode('Hello React 2')
+firstElementDom.appendChild(textDom)
+firstElementDom.appendChild(textDom2)
+
+document.getElementById('root-dom').appendChild(firstElementDom)
